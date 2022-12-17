7da8f73f5866a211c8b5fba013f7268f667fdfcf
djalmabarbosa <djalma.a.b.jr@usp.br>
2022-12-17 17:43:08 -0400

    teste

--- app.R
+++ app.R
@@ -1,6 +1,10 @@
 library(shiny)
+library(ggplot2)
+library(Hmisc)
+library(xtable)
+library(hwriter)
+library(MASS)
 
-# Define UI for dataset viewer app ----
 ui <- fluidPage(
   
   # Application title
@@ -17,7 +21,9 @@ ui <- fluidPage(
       uiOutput("datafiles"),
       
       uiOutput("dependent")
-
+      #                uiOutput("independent"),
+      #                uiOutput("control"),
+      #                uiOutput("subsetValue")
     ),
     
     mainPanel(
@@ -47,7 +53,7 @@ ui <- fluidPage(
 
 ## get a list of datasets
 cd<-getwd();
-
+source("./scripts/crosstabs.R")
 setwd(cd)
 
 if (file.exists("./datasetlist.csv") )
@@ -64,17 +70,14 @@ datafiles<-setNames(datafiles, datasetlist$title)
 
 emptymatch<-"."
 
-
-
-
-
-
-# Define server logic to summarize and view selected dataset -------------------------------------------
 server <- function(input, output) {
   
   output$datafiles <-renderUI({   
     selectInput("dataset", "Selecione o banco de dados:", choices = c(Nenhum=".", datafiles ), selected=".")
   }) 
+  
+  
+  
   
   getDataPath<- reactive({
     
@@ -120,6 +123,82 @@ server <- function(input, output) {
     
   })
   
+  output$independent<-renderUI({ 
+    
+    if (!is.null(input$dataset) & !identical(input$dataset, emptymatch ))
+    {
+      fullpath<-getDataPath()
+      getDatsetInfo(fullpath)                        
+      
+      selectInput("independent", 
+                  label = "Choose an independent variable ",
+                  choices<- c(Nenhum='.', varnames),
+                  selected = '.'
+      )
+    } else {
+      
+      selectInput("independent", 
+                  label = "Choose an independent variable ",
+                  choices= c(Nenhum='.'),
+                  selected = '.'
+      )
+      
+    }
+  })
+  
+  output$control<-renderUI({ 
+    
+    if (!is.null(input$dataset) & !identical(input$dataset, emptymatch )
+        
+    )
+    {
+      fullpath<-getDataPath()
+      getDatsetInfo(fullpath)
+      
+      selectInput("control", 
+                  label = "Choose a control variable ",
+                  choices<- c(Nenhum='.', varnames),
+                  selected = '.'
+      )
+    } else {
+      selectInput("control", 
+                  label = "Choose a control variable ",
+                  choices<- c(Nenhum='.'),
+                  selected = '.'
+      )
+      
+    }
+  })
+  
+  output$subsetValue<-
+    renderUI({
+      if ((!is.null(input$dataset) & !identical(input$dataset, emptymatch ))
+          & (!is.null(input$control) & !identical(input$control, emptymatch ))
+          
+      )
+      {
+        
+        
+        subset<-datasetname[[input$control]]
+        subsetValues<-names(table(subset[1]))
+        
+        selectInput("subsetValue", 
+                    label = "Choose a subset within the control",                   
+                    choices<- c(Nenhum='.', subsetValues),
+                    selected = '.'
+        )
+        
+      } else {
+        selectInput("subsetValue", 
+                    label = "Choose a subset within the control",
+                    choices<- c(Nenhum='.'),
+                    selected = '.'
+        )
+        
+      }
+      
+    })
+  ## Add something here to check for invalid combinations and return a message?)
   
   output$table <- renderTable({
     if (( !is.null(input$dataset) &  !identical(input$dataset, emptymatch) )
@@ -132,9 +211,16 @@ server <- function(input, output) {
       
       # For now if there is a control variable, subset based on the value selected
       # This is ugly but it works with xtable which shiny depends on.
- 
+      if (!is.null(input$subsetValue) & !identical(input$subsetValue, emptymatch) 
+          & !is.null(input$control) & !identical(input$control, emptymatch)
+      )
+      {
+        datasetnamec<-datasetname[datasetname[[input$control]] == input$subsetValue,]
+        
+      }
+      
       # Must be in the correct order.
-      factorsToUse<-c(input$dependent)
+      factorsToUse<-c(input$dependent, input$independent)
       
       factorsToUse<-factorsToUse[factorsToUse != "."]
       ctab<<-NULL
@@ -142,11 +228,29 @@ server <- function(input, output) {
       if (!exists("datasetnamec"))
       {
         
-        ctab<<-creatextab(factorsToUse, datasetname)
+        ctab<<-creatextab( factorsToUse, datasetname)
       } else {
-        ctab<-creatextab(factorsToUse, datasetnamec)
+        ctab<-creatextab( factorsToUse, datasetnamec)
       }
+      dimctab<-dim(ctab)
       
+      ndims<<-NULL
+      ndims<<-length(dimctab)
+      
+      if (ndims == 2)
+      {
+        cnames<-colnames(ctab)
+        ncols<-dimctab[2]
+        
+        ctab<-prop.table(ctab,2)*100
+        # Because of datasetc
+        ctab<<-ctab
+        
+        
+        
+      } else if (ndims == 1)
+      {
+        
         parte1 <- as.data.frame(prop.table(ctab)*100)
         parte2 <- as.data.frame(round(prop.table(ctab)*nrow(datasetname),digits = 0))
         
@@ -155,13 +259,56 @@ server <- function(input, output) {
         names(ctab) <- c("Categorias","Freq","N")
         
         ctab<<-ctab
-
+      }                 
+      
+      
       return(ctab)
       
     } 
-  
+    
   })
   
+  output$plot<-renderPlot({
+    
+    if (!is.null(input$dataset) & !identical(input$dataset, emptymatch ) & !is.null(ndims))
+    {
+      if (!identical(input$dependent, emptymatch ) & identical(input$independent, emptymatch ) 
+          & identical(input$subsetValue, emptymatch )
+          & identical(input$control, emptymatch )
+          
+          & ndims == 1)
+      {
+        plot<-barplot(ctab, beside = TRUE)
+      } else if ((ndims == 2 &  (!is.null(input$dependent) & !identical(input$dependent,emptymatch) )
+                  | (!is.null(input$independent) & !identical(input$independent,emptymatch) ))
+                 & (identical(input$subValue,emptymatch) | is.null(input$subValue))
+                 
+      ) 
+        
+      {
+        
+        plot<-barplot(ctab, beside = FALSE)
+        ndims<<-NULL
+        ctab<<-NULL
+      } else if (
+        ((!is.null(input$dependent) & !identical(input$dependent,emptymatch) )
+         & (!is.null(input$independent) & identical(input$independant,emptymatch))
+         & (!is.null(input$control) & !identical(input$control,emptymatch))
+         & !identical(input$subvalue,emptymatch) & ndims == 2)
+      )
+      {
+        
+        
+        ## Change this to show grouped plot?
+        plot<-barplot(ctab, beside = FALSE)
+        ndims<<-NULL
+        ctab<<-NULL
+      }
+      
+      plot
+    }                
+    
+  })
   
   output$summary<-renderText({
     
@@ -190,10 +337,28 @@ server <- function(input, output) {
     if (!identical(input$dependent, emptymatch) & !is.null(input$dependent) )
     {
       title<-paste0("<h4>",input$dependent)
+      
     }
-   })
+    
+    if (!is.null(input$independent) & !identical(input$independent, emptymatch))
+    {
+      title<-paste(title,"by", input$independent, "<br/>", sep=" ")
+      
+    }
+    if (!is.null(input$control) & !identical(input$control, emptymatch))
+    {
+      title<-paste0(title, "Controlling for ", input$control, " = ", input$subsetValue, "<br/>")      
+    }
+    if (title != "")
+    {
+      title<-paste0(title, " (%)</h4>")
+      title<-gsub("_", " ", title)
+    }
+    
+  })
   
 }   
+
 
 
 
