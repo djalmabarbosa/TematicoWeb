be52c4989d4ebe6d755a9cc7eca88f894f8a0520
djalmabarbosa <djalma.a.b.jr@usp.br>
2022-12-17 16:31:15 -0400

    teste

--- app.R
+++ app.R
@@ -3,48 +3,54 @@ library(shiny)
 # Define UI for dataset viewer app ----
 ui <- fluidPage(
   
-  # App title ----
-  titlePanel("Temático Web"),
+  # Application title
+  titlePanel("Projeto Temático"),
   
-  # Sidebar layout with a input and output definitions ----
   sidebarLayout(
-    
-    # Sidebar panel for inputs ----
     sidebarPanel(
+      tags$style(type="text/css",
+                 ".shiny-output-error { visibility: hidden; }",
+                 ".shiny-output-error:before { visibility: hidden; }"
+      ),
       
-      # Input: Selector for choosing dataset ----
-      selectInput(inputId = "dataset",
-                  label = "Escolha um banco de dados:",
-                  choices = c("Preliminar", "Primeira Onda")),
+      helpText("Escolha banco de dados e variável"),
+      uiOutput("datafiles"),
       
-      # Input: Numeric entry for number of obs to view ----
-      numericInput(inputId = "obs",
-                   label = "Number of observations to view:",
-                   value = 10)
+      uiOutput("dependent")
+
     ),
     
-    # Main panel for displaying outputs ----
     mainPanel(
+      htmlOutput("title"),
+      tabsetPanel(     
+        
+        tabPanel("Tabela",
+                 
+                 htmlOutput("table"),
+                 
+                 htmlOutput("summary")
+        ),
+        tabPanel("Questionário do Preliminar",
+                 includeMarkdown("./docs/QuestionarioPreliminar.Rmd")
+        ),
+        tabPanel("Questionário da Primeira Onda",
+                 includeMarkdown("./docs/QuestionarioPrimeiraOnda.Rmd")         
+        )
+        
+      )
       
-      # Output: Verbatim text for data summary ----
-      verbatimTextOutput("summary"),
-      
-      # Output: HTML table with requested number of observations ----
-      tableOutput("view")
-      
-    )
-  )
+    ))
 )
 
 
-## get a list of datasets ----------------------------------------------------------------
+# Lê bancos de dados e informações -------------------------------------------------------
 
 if (file.exists("./datasetlist.csv") )
 {
   ## datasetname, title, description
   datasetlist<-read.csv("./datasetlist.csv", stringsAsFactors = FALSE)
 } else {
-  "Dataset file is missing."
+  "Banco de dados ausente."
   return()
 }
 
@@ -57,86 +63,92 @@ emptymatch<-"."
 
 
 
-# Define server logic to summarize and view selected dataset ------------------------------
+
+# Define server logic to summarize and view selected dataset -------------------------------------------
 server <- function(input, output) {
   
-  # Return the requested dataset ----
   output$datafiles <-renderUI({   
-    selectInput("dataset", "Selecione os dados", choices = c(None=".", arquivos), selected=".")
+    selectInput("dataset", "Selecione o banco de dados:", choices = c(Nenhum=".", datafiles ), selected=".")
   }) 
   
   getDataPath<- reactive({
     
-    fullpath<-paste0('./dados/', input$dataset)
+    fullpath<-paste0('./data/', input$dataset)
   })
   
   getDatsetInfo<-function(fullpath)
   {
-    nomebanco<<-NULL
-    nomebanco<<-readRDS(fullpath)
+    datasetname<<-NULL
+    datasetname<<-readRDS(fullpath)
     
-    nomesvar<-colnames(nomebanco)
+    varnames<-colnames(datasetname)
     
-    nomesvar<<-NULL
-    nomesvar<<-nomesvar[nomesvar != "Freq"]
+    varnames<<-NULL
+    varnames<<-varnames[varnames != "Freq"]
     
   }
   
-  # Primeiro output: a escolha da variável
-  shinyServer(function(input, output) {
+  
+  
+  
+  output$dependent<-renderUI({
     
-    output$datafiles <-renderUI({   
-      selectInput("dataset", "Select your data", choices = c(None=".", datafiles ), selected=".")
-    }) 
-    getDataPath<- reactive({
-      fullpath<-paste0('./data/', input$dataset)
-    })
-    getDatsetInfo<-function(fullpath)
+    if (!is.null(input$dataset) & !identical(input$dataset, emptymatch ))
     {
-      datasetname<<-NULL
-      datasetname<<-readRDS(fullpath)
-      varnames<-colnames(datasetname)
-      varnames<<-NULL
-      varnames<<-varnames[varnames != "Freq"]
-    }
-    
-    output$dependent<-renderUI({
-      if (!is.null(input$dataset) & !identical(input$dataset, emptymatch ))
-      {
-        fullpath<-getDataPath()
-        getDatsetInfo(fullpath)                        
-        selectInput("dependent", 
-                    label = "Escolha uma variável",                   
-                    choices<- c(None='.', varnames),
-                    selected = '.'
-        )
-      } else {
-        selectInput("dependent", 
-                    label = "Escolha uma variável",
-                    choices <- c(None='.'),
-                    selected = '.'
-        )
-        
-      }
       
-    })
-  
-  
-  # Generate a summary of the dataset ----
-  output$summary <- renderPrint({
-    
-    if (( !is.null(input$dataset) &  !identical(input$dataset, emptymatch) )
-        & ( !identical(input$dependent, emptymatch) & !is.null(input$dependent) )
-    )
-    {    
       fullpath<-getDataPath()
-      getDatsetInfo(fullpath) 
-      summary(input$dependent)
+      getDatsetInfo(fullpath)                        
+      
+      selectInput("dependent", 
+                  label = "Escolha uma variável:",                   
+                  choices<- c(Nenhum='.', varnames),
+                  selected = '.'
+      )
+    } else {
+      selectInput("dependent", 
+                  label = "Escolha uma variável: ",
+                  choices<- c(Nenhum='.'),
+                  selected = '.'
+      )
+      
     }
+    
   })
   
-}
   
+  output$summary<-renderText({
+    
+    if (!is.null(input$dataset) & !identical(input$dataset, emptymatch))
+    {      
+      selectedData<-datasetlist[datasetlist$datasetname == input$dataset,]
+      #To start off these are set to null.
+      ndims<<-NULL
+      ctab<<-NULL
+      
+      text1<-paste("<h3> <br><br> Informações sobre este banco de dados</h3>")
+      ## Include an HTML description in the data file.
+      text4<-paste0("<h4>",as.character(selectedData["title"]), "</h4>")
+      text2<-as.character(selectedData["dataDescription"])
+      text3<-paste(text1, text4, text2, sep="<br><br> ")
+      
+      return(text3)
+    } else {
+      "<h5><p><br>Por favor, escolha um banco de dados.</p><h5>"
+      
+    }
+    
+  }) 
+  output$title<-renderText({
+    title<-""
+    if (!identical(input$dependent, emptymatch) & !is.null(input$dependent) )
+    {
+      title<-paste0("<h4>",input$dependent)
+    }
+   })
+  
+}   
+
+
 
 # Create Shiny app ----
 shinyApp(ui = ui, server = server)
